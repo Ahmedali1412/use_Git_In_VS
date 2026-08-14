ec282304609ca0e641b90717cddb85d268fc6f83
Ahmedali <ahmedxali25@gmail.com>
2026-08-15 01:09:17 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace use_Git_In_VS
+{
+    internal class Program
+    {
+        // add feature : print element of any int arrray. => commit it 
+        public void PrintList(IEnumerable<int> arr)
+        {
+            Console.WriteLine($"list of arr is:\n{string.Join(" ", arr)}");
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 6, 3, 4, 9, 2, 7, 8, 10, 5 };
+            Console.WriteLine($"list of arr is:\n{string.Join(" ", arr)}" + "\n" +
+                $"\nafter sorted asc is:\n{string.Join(" ", arr.OrderBy(x => x))}");
+        }
+    }
+}
